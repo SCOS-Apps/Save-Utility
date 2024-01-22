92bf2e524788d3a665370dddaba9b0a9647559a8
SCOS100 <hsc100zz@gmail.com>
2024-01-22 12:00:24 +0100

    new file:   .vscode/tasks.json new file:   __main__.py new file:   __pycache__/__read__.cpython-312.pyc new file:   __read__.py new file:   save.ini

---
+++ __main__.py
@@ -0,0 +1,29 @@
+import configparser
+import __read__ as read
+import sys
+
+config = configparser.ConfigParser()
+
+try:
+    sys.argv[2]
+except IndexError:
+    config.read(input("File Path: "))
+else:
+    print("MEM Set")
+
+print("SCOS Config & Save Editor and reader.")
+
+print("NOTE: Do NOT close the program while in write function.")
+
+print("Functions:\n1. Read\n2. Write\n3. Exit")
+
+selection = input("Selection: ")
+if selection == "1" or "read":
+    N1 = input("Define Class: ")
+    N2 = input("Define Class Variable: ")
+    print(read.read(N1, N2))
+elif selection == "2" or "write":
+    N1 = input("Define Class: ")
+    N2 = input("Define Class Variable: ")
+    N3 = input("Define Change: ")
+    read.write(N1, N2, N3)
---
+++ __read__.py
@@ -0,0 +1,16 @@
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+
+try:
+    sys.argv[1]
+except IndexError:
+    config.read(input("File Path: "))
+else:
+    print("MEM Set")
+
+def read(direct, variable):
+    print(config[direct][variable])
+def write(direct, variable, change):
+    config[direct][variable] = change
