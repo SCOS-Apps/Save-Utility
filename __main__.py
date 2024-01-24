c290c1bbc6e5e409c73a662f65a65101fd0f9ffa
SCOS100 <hsc100zz@gmail.com>
2024-01-24 12:59:37 +0100

    modified:   __main__.py 	modified:   __pycache__/__read__.cpython-312.pyc

--- __main__.py
+++ __main__.py
@@ -19,6 +19,9 @@ print("Functions:\n1. Read\n2. Write\n3. Exit")
 
 selection = input("Selection: ")
 if selection == "1" or "read":
+    global N1
+    global N2
+    global N3
     N1 = input("Define Class: ")
     N2 = input("Define Class Variable: ")
     print(read.read(N1, N2))
@@ -27,4 +30,3 @@ elif selection == "2" or "write":
     N2 = input("Define Class Variable: ")
     N3 = input("Define Change: ")
     read.write(N1, N2, N3)
-    print
