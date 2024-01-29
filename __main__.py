4ee1e733bc4cd3fbbf391ad17432e3285f224df9
Oscar Allegro <oscar.allegro@icmelissa.edu.it>
2024-01-29 12:30:26 +0100

    modified:   __main__.py

--- __main__.py
+++ __main__.py
@@ -13,21 +13,21 @@ print("NOTE: Do NOT close the program while in write function.")
 print("Functions:\n1. Read\n2. Write\n3. Exit")
 
 selection = input("Selection: ")
-if selection == "1" or "read":
+if (selection == "1"):
     N1 = input("Define Class: ")
     N2 = input("Define Class Variable: ")
-    if (N1 or N2 == None):
-        while N1 or N2 == None:
+    if (N1 == "") or (N2 == ""):
+        while (N1 == "") or (N2 == ""):
             print("Warning: Variables Are None.")
             N1 = input("Define Class: ")
             N2 = input("Define Class Variable: ")
     print(read.read(N1, N2))
-elif selection == "2" or "write":
+elif (selection == "2"):
     N1 = input("Define Class: ")
     N2 = input("Define Class Variable: ")
     N3 = input("Define Change: ")
-    if (N1 or N2 == None):
-        while N1 or N2 == None:
+    if (N1 == "") or (N2 == "") or (N3 == ""):
+        while (N1 == "") or (N2 == "") or (N3 == ""):
             print("Warning: Variables Are None.")
             N1 = input("Define Class: ")
             N2 = input("Define Class Variable: ")
