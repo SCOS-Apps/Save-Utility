073cab1724fa52e85b845ece4f3f5a6e6395e1f6
SCOS100Alt <oscar.allegro@icmelissa.edu.it>
2024-02-26 11:51:51 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='ssave',
       version='v1.0.3',
       description="SCOS Save Utility",
+      long_description=readme(),
       url='https://github.com/SCOS-Apps/Save-Utility',
       readme="README.md",
       keywords = ["save", "scos", "ssave"],
