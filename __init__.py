13ac8c2edf50b32e6f7049ac8dd6e34ca2b48a1f
SCOS100Alt <oscar.allegro@icmelissa.edu.it>
2024-02-26 11:19:42 +0100

    modified:   __init__.py 	deleted:    line.py

--- __init__.py
+++ __init__.py
@@ -6,3 +6,10 @@ config = configparser.ConfigParser()
 print("SCOS Save Utility is Included.")
 config.read("data.ini")
 print(config.read("Info", "Version"))
+
+try:
+    sys.argv[1]
+except IndexError:
+    print("No file in sys.argv[1].")
+else:
+    easy.sopen(sys.argv[1])
