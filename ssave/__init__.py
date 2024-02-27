f0b3c6e020da96da66f26e9cfdd03c6efd6e1d40
SCOS100Alt <158041890+SCOS100Alt@users.noreply.github.com>
2024-02-27 08:55:40 +0000

    modified:   ssave/__init__.py 	deleted:    ssave/easy.py

--- ssave/__init__.py
+++ ssave/__init__.py
@@ -1,11 +1,27 @@
 import sys
 import configparser
-import easy
 
 config = configparser.ConfigParser()
 print("SCOS Save Utility is Included.")
-config.read("data.ini")
-print(config.read("Info", "Version"))
+
+def read(direct, variable):
+    print(config[direct][variable])
+
+def write(direct, variable, change):
+    config[direct][variable] = change
+
+def sopen(file):
+    try:
+        print(file)
+    except:
+        print("File not found or not specified.")
+    else:
+        try:
+            config.read(file)
+        except:
+            print("File does not exist or something else happened.")
+        else:
+            config.read(file)
 
 try:
     print(sys.argv[1])
